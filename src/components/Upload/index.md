096f093130421870ab9e6700050d9fac3c2c1625
chanlumin <1025300632@qq.com>
2020-04-13 10:11:05 +0800

    feat(upload): 添加onchange，beforeUplpoad文件钩子函数

--- src/components/Upload/index.md
+++ src/components/Upload/index.md
@@ -16,12 +16,34 @@ const handleProgress = (percentage, file) => {
   console.log(`progress...%${percentage}`, file);
 };
 
+const checkSize = file => {
+  if (file.size > 1024 * 1024 * 5) {
+    alert('文件最大不能超过5M');
+    return false;
+  }
+  return true;
+};
+
+/**
+ * 修改文件名
+ **/
+const handleFilePromise = (file: File) => {
+  const newFile = new File([file], 'new File.docx', { type: file.type });
+  return Promise.resolve(newFile);
+};
+
+const handleChange = (file: File) => {
+  console.log(file);
+};
+
 export default () => (
   <Upload
     action="/api/posts"
+    beforeUpload={handleFilePromise}
     onSuccess={handleSuccess}
     onError={handleError}
     onProgress={handleProgress}
+    onChange={handleChange}
   />
 );
 ```
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -2,16 +2,25 @@ import React, { useRef, MouseEvent, ChangeEvent } from 'react';
 import axios from 'axios';
 
 interface UPloadProps {
+  beforeUpload?: (file: File) => boolean | Promise<File>;
   action: string;
   onSuccess?: (data: any, file: File) => void;
   onError?: (error: any, file: File) => void;
   onProgress?: (percentage: number, file: File) => void;
+  onChange?: (file: File) => void;
 }
 
 const Upload: React.FC<UPloadProps> = props => {
   const inputFileRef = useRef<HTMLInputElement>(null);
 
-  const { action, onSuccess, onError, onProgress } = props;
+  const {
+    action,
+    onSuccess,
+    onError,
+    onProgress,
+    beforeUpload,
+    onChange,
+  } = props;
 
   /**
    *
@@ -36,40 +45,68 @@ const Upload: React.FC<UPloadProps> = props => {
 
   /**
    * 处理文件上传流程
+   * 多文件上传遍历文件分次上传
    * @param files 上传文件类数组
    */
   const handleUploadFile = (files: FileList) => {
     const fileList = Array.from(files);
-    const formData = new FormData();
     fileList.forEach(file => {
-      formData.append(file.name, file);
-      axios
-        .post(action, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          // axios的上传过程钩子
-          onUploadProgress: (evt: ProgressEvent) => {
-            const percentage = Math.round((evt.loaded * 100) / evt.total) || 0;
-            if (percentage < 100) {
-              if (onProgress) {
-                onProgress(percentage, file);
-              }
-            }
-          },
-        })
-        .then(resp => {
-          console.log('resp..... : ', resp);
-          if (onSuccess) {
-            onSuccess(resp.data, file);
-          }
-        })
-        .catch(error => {
-          if (onError) {
-            onError(error, file);
-          }
-        });
+      if (!beforeUpload) {
+        request(file);
+      } else {
+        const result = beforeUpload(file);
+        // promise方式处理文件
+        if (result && result instanceof Promise) {
+          result.then(processedFile => {
+            request(processedFile);
+          });
+          // 规定处理为文件未true的时候才会继续进行后续的文件发送
+        } else if (result !== false) {
+          // 正常方式处理文件
+          request(file);
+        }
+      }
     });
+  };
+
+  const request = (file: File) => {
+    const formData = new FormData();
+    formData.append(file.name, file);
+    axios
+      .post(action, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        // axios的上传过程钩子
+        onUploadProgress: (evt: ProgressEvent) => {
+          const percentage = Math.round((evt.loaded * 100) / evt.total) || 0;
+          if (percentage < 100) {
+            if (onProgress) {
+              onProgress(percentage, file);
+            }
+          }
+        },
+      })
+      .then(resp => {
+        console.log('resp..... : ', resp);
+        if (onSuccess) {
+          onSuccess(resp.data, file);
+        }
+
+        // onChange钩子
+        if (onChange) {
+          onChange(file);
+        }
+      })
+      .catch(error => {
+        if (onError) {
+          onError(error, file);
+        }
+
+        if (onChange) {
+          onChange(file);
+        }
+      });
   };
 
   return (
